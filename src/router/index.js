21e2f2e018377a59506874b0d91c62cceb661ef6
alrimo <53372629+alrimo@users.noreply.github.com>
2020-08-03 19:05:18 -0400

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Test from "@/views/Test.vue";
 import Results from "@/views/TestResults.vue";
 import StudentName from "@/views/StudentName.vue";
 import TestName from "@/views/TestName.vue";
+import CreateTest from "@/views/CreateTest.vue";
 
 Vue.use(VueRouter);
 
@@ -40,6 +41,11 @@ const routes = [
     path: "/testName",
     name: "testName",
     component: TestName
+  },
+  {
+    path: "/create",
+    name: "create",
+    component: CreateTest
   }
 ];
 
---
+++ src/views/CreateTest.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+
+    </section>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+          mqf: null
+        }
+    },
+    mounted() {
+        if(localStorage.getItem('mqf')) {
+            try {
+              this.mqf = JSON.parse(localStorage.getItem('mqf'));
+            } catch(e) {
+              localStorage.removeItem('mqf');
+              console.log("Local MQF storage cleared");
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
